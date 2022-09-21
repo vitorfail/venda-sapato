cd39707af1d0b9cf59c6c95da183155c08bb36c3
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-21 16:15:46 -0300

    qeers

--- src/componentes/Painel/Painel.js
+++ src/componentes/Painel/Painel.js
@@ -2,12 +2,18 @@ import './Painel.css'
 import React, {useEffect, useState} from 'react'
 import Direita from '../../icons/direita.png'
 import Esquerda from '../../icons/esquerda.png'
-import Banner from '../../icons/banner.jpg'
+import Banner1 from '../../icons/banner1.jpg'
+import Banner2 from '../../icons/banner2.jpg'
+import Banner3 from '../../icons/banner3.png'
 
 
 export default function Painel(){
+    const [margemx, setmargemx]= useState(0)
     const [medidatotal, setmedidatotal] = useState(0)
     const [medidaBanner, setmedidaBanner] = useState(0)
+    function moverdireita(){
+        setmargemx(margemx-window.innerWidth)
+    }
     useEffect(() => {
         let largura = window.innerWidth * 3
         setmedidatotal(largura)
@@ -19,19 +25,19 @@ export default function Painel(){
                 <img alt='direito' src={Direita}></img>
             </div>
             <div className='esquerda'>
-                <img alt='esquerda' src={Esquerda}></img>
+                <img alt='esquerda' src={Esquerda} onClick={() => moverdireita()}></img>
             </div>
 
             <div className='banner'>
-                <div className='container' style={{width:medidatotal}}>
+                <div className='container' style={{width:medidatotal, marginLeft:margemx}}>
                     <div className='imagem' style={{width:medidaBanner}}>
-                        <img src={Banner}></img>
+                        <img  alt='banner' src={Banner1}></img>
                     </div>
                     <div className='imagem' style={{width:medidaBanner}}>
-                        <img src={Banner}></img>
+                        <img  alt='banner' src={Banner2}></img>
                     </div>
                     <div className='imagem' style={{width:medidaBanner}}>
-                        <img src={Banner}></img>
+                        <img  alt='banner' src={Banner3}></img>
                     </div>
                 </div>
             </div>
