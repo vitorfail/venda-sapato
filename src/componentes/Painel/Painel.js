d5949fef7cfa23bbbf2cbae6afdff503b7a2f9e3
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-22 19:18:18 -0300

    1234

--- src/componentes/Painel/Painel.js
+++ src/componentes/Painel/Painel.js
@@ -1,56 +1,41 @@
 import './Painel.css'
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import Direita from '../../icons/direita.png'
 import Esquerda from '../../icons/esquerda.png'
-import Banner1 from '../../icons/banner1.jpg'
+import Banner1 from '../../icons/banner1.png'
 import Banner2 from '../../icons/banner2.jpg'
 import Banner3 from '../../icons/banner3.png'
 
 
 export default function Painel(){
-    const [margemx, setmargemx]= useState(0)
-    const [medidatotal, setmedidatotal] = useState(0)
-    const [medidaBanner, setmedidaBanner] = useState(0)
-    const [dados,setdados ] = useState([ <div key={1} className='imagem' style={{width:"100vw"}}>
-                        <img  alt='banner' src={Banner1}></img>
-                    </div>,
-                    <div key={2} className='imagem' style={{width:"100vw"}}>
-                        <img  alt='banner' src={Banner2}></img>
-                    </div>,
-                    <div key={3} className='imagem' style={{width:"100vw"}}>
-                        <img  alt='banner' src={Banner3}></img>
-                    </div>])
-    const [index, setindex] = useState(dados.length)
-    function moverdireita(){
-        if(index === 2){
-            console.log('passou aqui')
-            let i = dados.length-1
-            let penultimadiv = dados[i]
-            let divcompleta =dados
-            divcompleta.splice(i, 1)
-            setdados(divcompleta.append(penultimadiv))
-        }
-        setindex(index-1)
+    const [um, setum]= useState('imagem')
+    const [dois, setdois]= useState('imagem')
+    const [tres, settres]= useState('imagem')
 
-        setmargemx(margemx-window.innerWidth)
+    function moverdireita(){
+        setum('imagem ative')
+        console.log('passou aqui')
     }
-    useEffect(() => {
-        let largura = window.innerWidth * 3
-        setmedidatotal(largura)
-        setmedidaBanner(window.innerWidth)
-    }, [setmedidatotal, setmedidaBanner])
     return(
         <div className='painel'>
             <div className='direita'>
-                <img alt='direito' src={Direita}></img>
+                <img alt='direito' src={Direita} onClick={() => moverdireita()}></img>
             </div>
             <div className='esquerda'>
-                <img alt='esquerda' src={Esquerda} onClick={() => moverdireita()}></img>
+                <img alt='esquerda' src={Esquerda} ></img>
             </div>
 
             <div className='banner'>
-                <div className='container' style={{width:medidatotal, marginLeft:margemx}}>
-                    {dados}
+                <div className='container'>
+                    <div key={1} className={um}>
+                        <img  alt='banner' src={Banner1}></img>
+                    </div>
+                    <div key={2} className={dois}>
+                        <img  alt='banner' src={Banner2}></img>
+                    </div>
+                    <div key={3} className={tres}>
+                        <img  alt='banner' src={Banner3}></img>
+                    </div>
                 </div>
             </div>
         </div>
