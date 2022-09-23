fabb96296c6006b0ddef624c8fbac5844ce274a5
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-23 12:24:05 -0300

    trato

--- src/componentes/Painel/Painel.js
+++ src/componentes/Painel/Painel.js
@@ -5,49 +5,71 @@ import Esquerda from '../../icons/esquerda.png'
 import Banner1 from '../../icons/banner1.png'
 import Banner2 from '../../icons/banner2.jpg'
 import Banner3 from '../../icons/banner3.png'
-
+import Banner4 from '../../icons/banner4.jpg'
+import Banner5 from '../../icons/banner5.jpg'
 
 export default function Painel(){
     const ref = useRef([]);
-    const array = [0, 1, 2]
-    const imagens = [Banner1, Banner2, Banner3]
+    const array = [0, 1, 2, 3, 4]
+    const imagens = [Banner1, Banner2, Banner3, Banner4, Banner5]
+    const [ ativodireita, setativodireita] = useState(false)
     const [i, seti] = useState(0)
 
     function moverdireita(){
-        if(i === array.length-2){
-            ref.current[0].className = 'imagem preparo'
-            var atual = i
-            var proxima = atual+1
-            ref.current[proxima].className = 'imagem ative'
-            ref.current[atual].className = 'imagem saindo'
-            setTimeout(() =>ref.current[atual].className = 'imagem', 700)
-            seti(i+1)
-        }
-        else{
-            if(i === array.length-1){
-                console.log('passou aqui')
-                ref.current[0].className = 'imagem ative'
-                ref.current[i].className = 'imagem saindo'
-                setTimeout(() =>ref.current[i].className = 'imagem', 700)
-
+        if(ativodireita === false){
+            setativodireita(true)
+            if(i+1 === imagens.length){
+                ref.current[i].style.transform ='translate3d(-300%, 0,0)'
+                ref.current[0].style.transform ='translate3d(0%, 0,0)'
+                setTimeout(() =>ref.current[i].style.display ='none', 400)
+                setTimeout(() =>ref.current[i].style.transform ='translate3d(100%, 0,0)', 500)
+                setTimeout(() =>ref.current[i].style.display ='block', 900)
                 seti(0)
+                console.log('passou aqui')
             }
             else{
-                var atual = i
-                var proxima = atual+1
-                ref.current[proxima].className = 'imagem ative'
-                ref.current[atual].className = 'imagem saindo'
-                setTimeout(() =>ref.current[atual].className = 'imagem', 700)
-                seti(i+1)
-                console.log(i)    
+                if(i !== 0){
+                    console.log(imagens.length)
+                    console.log(i)
+                    if(i+2 === imagens.length){
+                        let dist = (100*(i+1))
+                        ref.current[i+1].style.transform ='translate3d(-'+dist+'%, 0,0)'
+                        setTimeout(() => ref.current[i].style.transform ='translate3d(-'+dist+'%, 0,0)', 100)
+                        setTimeout(() => ref.current[0].style.transform ='translate3d(0%, 0,0)' , 500)
+                        setTimeout(() =>ref.current[i].style.transform ='translate3d(100%, 0,0)', 500)
+                        seti(i+1)    
+        
+                    }
+                    else{
+                        let dist = (100*(i+1))
+                        ref.current[i+1].style.transform ='translate3d(-'+dist+'%, 0,0)'
+                        setTimeout(() => ref.current[i].style.transform ='translate3d(-'+dist+'%, 0,0)', 100)
+                        setTimeout(() =>ref.current[i].style.transform ='translate3d(100%, 0,0)', 500)
+                        seti(i+1)        
+                    }
+                }
+                else{
+                    ref.current[0].style.transform ='translate3d(-100%, 0,0)'
+                    ref.current[0+1].style.transform ='translate3d(-100%, 0,0)'
+                    setTimeout(() =>ref.current[0].style.transform ='translate3d(100%, 0,0)', 500)
+                    seti(i+1)  
+                }
             }
+            setTimeout(() => { setativodireita(false)}, 1000);
         }
     }
+    function moveresquerda(){
+
+        let dist = (-100*(i-1))
+        ref.current[i].style.transform ='translate3d('+dist+'%, 0,0)'
+        setTimeout(() => { ref.current[i-1].style.transform ='translate3d('+dist+'%, 0,0)'}, 500)
+        seti(i-1)    
+
+    }
     useEffect(() => {
-        if(i ===0 ){
-            ref.current[0].className = 'imagem ative'
-            ref.current[1].className = 'imagem preparo'
+        if(i === 0){
         }
+
     })
     return(
         <div className='painel'>
@@ -55,7 +77,7 @@ export default function Painel(){
                 <img alt='direito' src={Direita} onClick={() => moverdireita()}></img>
             </div>
             <div className='esquerda'>
-                <img alt='esquerda' src={Esquerda} ></img>
+                <img alt='esquerda' src={Esquerda} onClick={() => moveresquerda()} ></img>
             </div>
             <div className='banner'>
                 <div className='container'>
