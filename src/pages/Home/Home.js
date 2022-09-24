cce98292100470fdf6a696b1e547638b2de0338c
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-24 15:06:45 -0300

    123

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,9 +9,19 @@ export default function Home(){
             <Cabecalho></Cabecalho>
             <Painel></Painel>
             <div className='quem'>
-                <h1><strong>Quem somos</strong></h1>
-                <h3>Somos um emmpresa marcada pela qualidade dos nossos produtos 
-                    e em clientes manter nossos clientes contantemente satisfeitos</h3>
+                <dv className='titulo'>
+                    <h1><strong>Quem somos</strong></h1>
+                </dv>
+                <div className='info'>
+                    <h3>Netshoes é uma empresa brasileira de comércio eletrônico de artigos esportivos fundada em 
+                        fevereiro de 2000 por Marcio Kumruian e Hagop Chabab e adquirido pela Magazine Luiza em 2019. 
+                        A Netshoes possui centros de distribuições no Brasil, e administra as lojas oficiais dos clubes 
+                        de futebol como Santos Futebol Clube, Corinthians, São Paulo Futebol Clube, Palmeiras, Internacional, 
+                        Cruzeiro, Vasco da Gama, Coritiba, Cruz Azul, Chivas, Monterrey, River Plate, San Lorenzo e América do México. 
+                        É também representante oficial dos produtos da National Basketball Association (NBA) na América Latina e responsável 
+                        pela administração das lojas virtuais das marcas Puma, Havaianas, Globo Esporte, Oakley, Timberland,
+                         Topper, Kappa Mizuno e UFC.[6][7] Também trabalha com calçados,acessórios para esportes e eletrônicos.</h3>
+                </div>
             </div>
         </div>
     )
